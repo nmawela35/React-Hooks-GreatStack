16901b45f5ae41348d223451353d8c7f5d85cfc2
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-24 16:11:38 +0200

    useCallback completed

---
+++ 05useCallback/src/Tutorial/UseCallback01.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+// function UseCallback01() {
+//     const [count,setCount]=useState(0)
+
+//   return (
+//     <>
+//     <Header/>
+//     <h1>{count}</h1>
+//     <button onClick={() =>{setCount(prev => prev+1)}}>Click Here</button>
+//     </>
+//   )
+// }
+
+export default UseCallback01
