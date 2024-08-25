9cd8a85bb14fb615346c014fc701713dc23f08d2
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-25 09:16:21 +0200

    useReducer completed, need revision

--- 07useReducer/src/App.jsx
+++ 07useReducer/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useReducer } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const initValue = {count: 0}
+  
+  const reducer =(state,action) =>{
+    if (action.type == "Increment")
+      {
+      return {count: state.count + action.payload}
+    }
+    if (action.type == "Decrement")
+      {
+      return {count: state.count - action.payload}
+    }
+    if(action.type == "input"){
+      return {count: action.payload}
+    }
+  }
+
+  const [state,dispatch] =useReducer(reducer,initValue)
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>useState</h1>
+    <h3>{count}</h3>
+    <button onClick={() =>setCount((prev) =>prev+1)}>Increase</button>
+    <button onClick={() =>setCount((prev) =>prev-1)}>Decrease</button>
+
+    <h1>useReducer</h1>
+    <h3>{state.count}</h3>
+    <button onClick={() =>dispatch({type:"Increment", payload:2})}>Increase</button>
+    <button onClick={() =>dispatch({type:"Decrement", payload:2})}>Decrease</button><br/><br/>
+    <input type="number" value={state.count} onChange={(e) =>dispatch({type:'input',payload:Number(e.target.value)})}/> 
+    
+    </>    
   )
 }
 
