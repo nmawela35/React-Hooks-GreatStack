b19df034481bb37b29d35d49ad9c2e892be6106e
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-25 09:45:10 +0200

    useLayouteffect completed

--- 08useLayouteffect/src/App.jsx
+++ 08useLayouteffect/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() =>{
+    console.log("useEffect")
+  },[])
+
+  useLayoutEffect(() =>{
+    console.log("useLayoutEffect")
+  },[])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>Test Message</h2>
+    {Array(40000).fill('').map((item,index) =>(
+      <li key={index}>{Math.pow(Math.random(), 10)}</li>
+    ))}
+    
     </>
   )
 }
